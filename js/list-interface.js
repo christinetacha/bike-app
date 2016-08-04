5a046a86c78e30dd4cc07a66821d7629169375c4
Christine Tacha and mollykmcglone <mollykmcglone@>
2016-08-04 12:04:25 -0700

    integrate list and search objects/prototypes

--- js/list-interface.js
+++ js/list-interface.js
@@ -1,6 +1,8 @@
 var List = require('./../js/list.js').listModule;
 
 $(document).ready(function() {
+  $('#seeList').click(function(event){
   var listObject = new List();
   listObject.getList();
+  });
 });
